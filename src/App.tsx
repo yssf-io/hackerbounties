bbabc9d8a7bf382a745ec779eec41af2f3d48ce0
yssf-io <hey@yssf.io>
2024-09-27 12:27:48 +0700

    added rainbowkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "./App.css";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
 
 function App() {
+  const account = useAccount();
+
+  useEffect(() => {
+    console.log({ account });
+  }, []);
+
   return (
-    <>
-      <h1 className="text-3xl font-light">hackfolio</h1>
-    </>
+    <div className="h-screen">
+      <h1 className="text-3xl pt-6 font-light">hackchat</h1>
+
+      {account.isConnected ? (
+        <div>user connected</div>
+      ) : (
+        <div className="m-auto mt-36 w-fit">
+          <ConnectButton />
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-createRoot(document.getElementById('root')!).render(
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "hackchat",
+  projectId: "154c55bd97004f4320be91936e2d9fbc",
+  chains: [mainnet],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
-)
+);
